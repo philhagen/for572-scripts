314d83e5c8be440dc6aca041c83eb78b2f713f55
Phil Hagen <phil@lewestech.com>
2019-11-09 12:50:07 +0100

    add workbook update alias

--- for572-aliases.sh
+++ for572-aliases.sh
@@ -1,2 +1,3 @@
 alias NetworkMiner="mono /usr/local/for572/NetworkMiner/NetworkMiner.exe"
 alias squidtime="awk '{\$1=strftime(\"%F %T\", \$1, 1); print \$0}'"
+alias workbook-update="/bin/bash /var/www/html/resources/workbook-update.sh"
